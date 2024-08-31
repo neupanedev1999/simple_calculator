f1d6a96d64aa84b8100e08bf6603ed3b7f1cfd1b
neupanedev1999 <neupanedev2055@gmail.com>
2024-08-31 22:11:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // script.js
+
 function appendToDisplay(value) {
     document.getElementById("display").value += value;
 }
@@ -20,3 +21,23 @@ function calculate() {
         alert("Invalid input");
     }
 }
+
+function handleKeyboardInput(event) {
+    const key = event.key;
+
+    if (key >= '0' && key <= '9') {
+        appendToDisplay(key);
+    } else if (key === '.') {
+        appendToDisplay('.');
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+        appendToDisplay(key);
+    } else if (key === 'Enter') {
+        calculate();
+    } else if (key === 'Backspace') {
+        deleteLast();
+    } else if (key === 'Escape') {
+        clearDisplay();
+    }
+}
+
+document.addEventListener('keydown', handleKeyboardInput);
